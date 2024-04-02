a1f36f81031503869775020d280b6928c154290c
Marlon <marlon.john@hotmail.de>
2024-04-02 06:04:35 +0200

    playground update

--- src/EpicManifestParser.Playground/Program.cs
+++ src/EpicManifestParser.Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Diagnostics;
+using System.Net;
 
 using BenchmarkDotNet.Attributes;
 
@@ -14,28 +15,42 @@ using ZlibngDotNet;
 
 var client = new HttpClient(new HttpClientHandler
 {
+	UseCookies = false,
 	UseProxy = false,
-	UseCookies = false
-});
+	AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+	MaxConnectionsPerServer = 256
+})
+{
+	DefaultRequestVersion = new Version(1, 1),
+	DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact,
+	Timeout = TimeSpan.FromSeconds(30)
+};
+
+var zlibng = new Zlibng(Benchmarks.ZlibngPath);
+
+var options = new ManifestParseOptions
+{
+	Zlibng = zlibng,
+	Client = client,
+	ChunkBaseUrl = "http://cloudflare.epicgamescdn.com/Builds/Fortnite/CloudDir/",
+	ChunkCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "chunks_v2")).FullName,
+	ManifestCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "manifests_v2")).FullName
+};
 
 using var manifestResponse = await client.GetAsync("https://media.wtf/XlQk.json");
 var manifestInfo1 = await manifestResponse.Content.ReadManifestInfoAsync();
 var manifestInfo2 = await ManifestInfo.DeserializeFileAsync(Benchmarks.ManifestInfoPath);
 
+var manifestInfoTuple = await manifestInfo2!.DownloadAndParseAsync(options);
+var parseResult = manifestInfoTuple.InfoElement.TryParseVersionAndCL(out var infoVersion, out var infoCl);
+
 var randomGuid = FGuid.Random();
 var chunkGuid = new FGuid("A76EAD354E9F6F06D0E75CAC2AB1B56C");
 
-var zlibng = new Zlibng(Benchmarks.ZlibngPath);
 var manifestBuffer = await File.ReadAllBytesAsync(Benchmarks.ManifestPath);
 
 var sw = Stopwatch.StartNew();
-var manifest = FBuildPatchAppManifest.Deserialize(manifestBuffer, options =>
-{
-	options.Zlibng = zlibng;
-	options.Client = client;
-	options.ChunkBaseUrl = "http://epicgames-download1.akamaized.net/Builds/Fortnite/CloudDir/";
-	options.ChunkCacheDirectory = Path.Combine(Benchmarks.DownloadsDir, "chunks_v2");
-});
+var manifest = FBuildPatchAppManifest.Deserialize(manifestBuffer, options);
 sw.Stop();
 Console.WriteLine(Math.Round(sw.Elapsed.TotalMilliseconds, 0));
 
@@ -50,26 +65,35 @@ Console.WriteLine(Math.Round(sw.Elapsed.TotalMilliseconds, 0));
 {
 	var fileManifest = manifest.FileManifestList.First(x =>
 		x.Filename.EndsWith("/pakchunk0optional-WindowsClient.ucas", StringComparison.Ordinal));
+	var fileManifestFileName = Path.GetFileName(fileManifest.Filename);
 	var fileManifestStream = fileManifest.GetStream();
-	await fileManifestStream.SaveFileAsync(Path.Combine(Benchmarks.DownloadsDir, "pakchunk0optional-WindowsClient.ucas"));
-	var fileBuffer = await fileManifestStream.SaveBytesAsync();
+	//await fileManifestStream.SaveFileAsync(Path.Combine(Benchmarks.DownloadsDir, "pakchunk0optional-WindowsClient.ucas"));
+	var fileBuffer = await fileManifestStream.SaveBytesAsync(ProgressCallback, fileManifestFileName);
+
 	FSHAHash.TryCompute(fileBuffer, out var hash);
+	Console.WriteLine(hash);
+	await fileManifestStream.SaveToAsync(new MemoryStream(fileBuffer, true), ProgressCallback, fileManifestFileName);
+
+	static void ProgressCallback(SaveProgressChangedEventArgs eventArgs)
+	{
+		var fileName = (string)eventArgs.UserState!;
+		Console.WriteLine($"{fileName}: {eventArgs.ProgressPercentage}% ({eventArgs.BytesSaved}/{eventArgs.TotalBytesToSave})");
+	}
 }
 
-var chunkUrl = $"http://epicgames-download1.akamaized.net/Builds/Fortnite/CloudDir/ChunksV4/{testChunk.GroupNumber:D2}/{testChunk.Hash:X16}_{testChunk.Guid}.chunk";
+var chunkUrl = $"http://cloudflare.epicgamescdn.com/Builds/Fortnite/CloudDir/ChunksV4/{testChunk.GroupNumber:D2}/{testChunk.Hash:X16}_{testChunk.Guid}.chunk";
 Console.WriteLine(chunkUrl);
-await Task.Delay(-1);
+Console.ReadLine();
 
 [MemoryDiagnoser(false)]
 public class Benchmarks
 {
 	public static string DownloadsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
-	public static string DocumentsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Documents");
 
 	public static string ManifestPath = Path.Combine(DownloadsDir, "jDihDvwDD4VfI5Ss7Uy-BNIY91lqSw.manifest");
 	//public static string ManifestPath = Path.Combine(DownloadsDir, "apk_manifest.json");
 	//public static string ManifestPath = Path.Combine(DownloadsDir, "last_pc_manifest.json");
-	public static string ZlibngPath = Path.Combine(DocumentsDir, @"Libraries\zlib-ng2.dll");
+	public static string ZlibngPath = Path.Combine(DownloadsDir, "zlib-ng2.dll");
 	public static string ManifestInfoPath = Path.Combine(DownloadsDir, "manifestinfo.json");
 
 	private byte[] _manifestBuffer = null!;
@@ -89,7 +113,7 @@ public class Benchmarks
 		{
 			options.Zlibng = _zlibng;
 			options.ChunkBaseUrl = "http://epicgames-download1.akamaized.net/Builds/Fortnite/CloudDir/";
-			options.ChunkCacheDirectory = Path.Combine(DownloadsDir, "chunks_v2");
+			options.ChunkCacheDirectory = Directory.CreateDirectory(Path.Combine(DownloadsDir, "chunks_v2")).FullName;
 		});
 		var fileManifest = manifest.FileManifestList.First(x =>
 			x.Filename.EndsWith("/pakchunk0optional-WindowsClient.ucas", StringComparison.Ordinal));
