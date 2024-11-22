130191e2376b3696776722c6d832d6e41ee916be
Marlon <marlon.john@hotmail.de>
2024-11-22 16:51:12 +0100

    added ZlibngDotNetDecompressor project

---
+++ src/EpicManifestParser.ZlibngDotNetDecompressor/ManifestZlibngDotNetDecompressor.cs
@@ -0,0 +1,23 @@
+﻿using ZlibngDotNet;
+
+namespace EpicManifestParser.ZlibngDotNetDecompressor;
+
+/// <summary>
+/// A decompressor using <see cref="Zlibng"/>.
+/// </summary>
+public static class ManifestZlibngDotNetDecompressor
+{
+	/// <summary>
+	/// Decompresses data buffer into destination buffer.
+	/// </summary>
+	/// <returns><see langword="true"/> if the decompression was successful; otherwise, <see langword="false"/>.</returns>
+	public static bool Decompress(object? state, byte[] source, int sourceOffset, int sourceLength, byte[] destination, int destinationOffset, int destinationLength)
+	{
+		var zlibng = (Zlibng)state!;
+
+		var result = zlibng.Uncompress(destination.AsSpan(destinationOffset, destinationLength),
+			source.AsSpan(sourceOffset, sourceLength), out int bytesWritten);
+
+		return result == ZlibngCompressionResult.Ok && bytesWritten == destinationLength;
+	}
+}
